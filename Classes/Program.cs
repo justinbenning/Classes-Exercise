9fcf6206a04adb5252bddaf811011c0bd0953327
JustinBenning <justin.benning@gmail.com>
2024-01-26 12:19:15 -0500

    updated exercise

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -10,22 +10,6 @@
 
         }
     }
-
-    public class Car
-    {
-        
-        public Car(string make, string model, int year)
-        {
-            Make = make;
-            Model = model;
-            Year = year;
-        }
-        public string Make { get; set; }
-        public string Model { get; set; }
-
-        public int Year { get; set; }
-    }
-    
 }
 
 
